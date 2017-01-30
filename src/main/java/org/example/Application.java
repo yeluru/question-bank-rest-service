e07b5bc1e68f0ee44a97e69c335075d866cb190a
Yeluru <g8urcy@osx-80jvg8wm-u.home>
2017-01-30 17:57:14 -0500

    based on video Bootstrapping a Spring Boot Application Project on lean stacks

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot starting point
+ *
+ */
+
+@SpringBootApplication
+public class Application 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	SpringApplication.run(Application.class, args);
+    }
+}
